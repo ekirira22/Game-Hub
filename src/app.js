3a5a2f0a7bf065e95a8f938d41726c051e391672
Eric Maranga <ekirira22@gmail.com>
2024-01-17 20:28:57 +0300

    added featured game section, to randomize a game

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const init = () =>{
         //STEP 2 - FETCH AND DISPLAY ALL THE GIVEAWAYS
         //Fetch all the give-aways and display them
     giveAways()
-        //STEP 3 - FETCH AND DISPLAY ALL GAMES FROM API URL
+        //STEP 3 - FETCH AND DISPLAY ALL GAMES FROM API URL - DISPLAY A RANDOM GAME AT FIRST, WHEN A USER SELECTS VIEW DISPLAY THAT GAME
         //STEP 4 - POST THAT DATA TO LOCAL DB.JSON ALONG WITH THE AVAILABLE PRE-ORDERS AND CURRENT ORDERS
         //STEP 5 - STORE THE GAME TO SESSIONSTORAGE TO PREVENT DATA FROM BEING LOST AFTER REFRESH AND DISPLAY THE 
         //          GAME ON THE FEATURED GAME SESSION -- PATCH,DELETE,POST WILL BE DONE ON LOCAL JSON
@@ -17,6 +17,42 @@ const init = () =>{
     featuredGame()
     
 
+}
+function featuredGame(){
+        //Display a random game from the API
+        //If sessionStorage is '' display random game . If it is set - Display that selected by user
+    const url = `${apiURL}games`
+    const method = 'GET'
+    const randomNumber = Math.floor(Math.random() * 396)
+    const featured = document.querySelector('div#game')
+
+    const response = processData(url,method)
+        //Display random game on page
+    response.then(res => {
+        const fGame = document.createElement('div')
+        fGame.className = 'featured-game'
+        fGame.innerHTML = `
+            <img src="${res[randomNumber].thumbnail}" alt="image" class="w-full">
+            <div class="m-4 relative">
+                <p class="text-center">Game Details:</p>
+                <p class="font-bold">${res[randomNumber].title}</p>
+                <p class=" text-gray-500 text-sm">${res[randomNumber].platform} </p> <br>
+                <p>Description: ${res[randomNumber].short_description}</p><br>
+                <div class="text-sm text-cyan-500 space-y-2">
+                    <p>Publisher: ${res[randomNumber].publisher}</p>
+                    <p>Developer: ${res[randomNumber].developer}</p>
+                    <p>Release Date: ${res[randomNumber].release_date}</p>
+                </div>
+                <div class="flex justify-between mt-4">
+                <button class="play-btn" id="play"><a href="">PRE-ORDER : <span id="count">${randomNumber}</span></a></button><span class="text-gray-500 text-sm">Limited Time Offer!!</span>
+                <button class="delete-btn" id="delete"><a href="">Not Interested</a></button>
+                </div>
+            </div>
+        `
+        
+        featured.appendChild(fGame)
+
+    })
 }
 
 function displayGameCards(){
@@ -78,7 +114,7 @@ function giveAways(){
 }
 
 /************** REUSABLE FUNCTIONS ****************************/
-    //FUNCTION THAT FETCHES DATA and returns 
+    //FUNCTION THAT FETCHES API DATA and returns 
 async function processData(url,method,options=null){
     const config = {
         method : method,
@@ -97,7 +133,8 @@ async function processData(url,method,options=null){
     }catch(error){
         console.error(error.message)
     }
-}
+}/*********************** EVENT LISTENER FUNCTIONS *************/
+
 
 /******************** MAIN EVENT LISTENER *********************/
 
