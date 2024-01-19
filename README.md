71e910d2defc5da14b7462ee4f010fbc1ba62e1b
Eric Maranga <ekirira22@gmail.com>
2024-01-19 10:18:58 +0300

    added extra genres for category, updated README.md

--- README.md
+++ README.md
@@ -11,25 +11,17 @@ Eric Maranga - Moringa School
       #### NB:
       Whenever you make a change to css remember to compile it using <code> npm run build-css</code> . See tailwind.config.js for more
 
+### Tech Stack
+   HTML,Tailwind CSS, Vanilla JS
+
 ### User Stories
  1. As a user you can be able to see all available live games in  database
-
-    <code>GET https://www.mmobomb.com/api1/games </code>
  2. As a user you can search games by platform eg. PC or Browser Platform
- 
-    <code>GET https://www.mmobomb.com/api1/games?platform=pc </code>
  3. A user can return details from a specific game selected from the available display of existing games
- 
-    <code>GET https://www.mmobomb.com/api1/game?id=452 </code>
  4. A user can see live MMO Giveaways and capacity remaining
- 
-    <code>GET https://www.mmobomb.com/api1/giveaways </code>
  5. A user can see like and unlike a game
- 
-    <code>GET https://www.mmobomb.com/api1/latestnews </code>
-
-6. A user add a pre-order a game and it reduces pre-orders reamining
-7. A user can dismiss a game from the featured game and a new randomized game is displayed
+ 6. A user add a pre-order a game and it reduces pre-orders reamining
+ 7. A user can dismiss a game from the featured game and a new randomized game is displayed
 
 ### API URL
 Click here to see the API URL [API URL link](https://www.mmobomb.com/api1/games)
--- src/app.js
+++ src/app.js
@@ -1,25 +1,119 @@
 //ALGORITHMIC PROCESS
-const apiURL = 'https://mmo-games.p.rapidapi.com/'
-const localAPI = 'http://127.0.0.1:3000/games'
-
-const init = () =>{
         //STEP 1
         //FETCH ALL THE GAMES FROM URL AND DISPLAY THEM
-    displayGameCards()
         //STEP 2 - FETCH AND DISPLAY ALL THE GIVEAWAYS
-        //Fetch all the give-aways and display them
-    giveAways()
         //STEP 3 - FETCH AND DISPLAY ALL GAMES FROM API URL - DISPLAY A RANDOM GAME AT FIRST, WHEN A USER SELECTS VIEW DISPLAY THAT GAME
         //STEP 4 - POST THAT DATA TO LOCAL DB.JSON ALONG WITH THE AVAILABLE PRE-ORDERS AND CURRENT ORDERS
         //STEP 5 - STORE THE GAME TO SESSIONSTORAGE TO PREVENT DATA FROM BEING LOST AFTER REFRESH AND DISPLAY THE 
         //          GAME ON THE FEATURED GAME SESSION -- PATCH,DELETE,POST WILL BE DONE ON LOCAL JSON
         //STEP 6 - ADD EVENT LISTENERS TO PRE-ORDER AND DELETE BUTTONS AND PERSIST WITH DATABASE
+        
+const apiURL = 'https://mmo-games.p.rapidapi.com/'
+const init = () =>{
+        //Fetch all games and append them to cards
+    displayGameCards()
+        //Fetch all the give-aways and display them
+    giveAways()
+        //Display the feature game, or user-selected game on the featured game section
     featuredGame()
 }
+    /* Game Cards that display individual games */
+    /* This function will display game cards, and also sort according to platform */
+function displayGameCards(platform=null,category=null){
+        //By default this function displays all games from index 0 - 395 in the main DOM
+    const method = 'GET'
+    let url
+    let gamesList
+        //If platform or category is null or if bothe have a value, allow it to fetch all games from index 0 - 395
+    if(platform !== null && category === ''){
+        let options = platform
+        url = `${apiURL}games?platform=${platform}`
+        gamesList = processData(url,method,options)
+    }else if(category !== null && platform === ''){
+        url = `${apiURL}games?category=${category}`
+        gamesList = processData(url,method)
+    }else if(platform !== null && category !== null){
+        url = `${apiURL}games?platform=${platform}&category=${category}`
+        gamesList = processData(url,method)
+    }else{
+        url = `${apiURL}games`
+        gamesList = processData(url,method)
+    }
+        //Display games on the main page
+    const gameCard = document.querySelector('div#gamecard')
+    gamesList.then(games => {
+        games.forEach(game => {
+            const card = document.createElement('div')
+            card.className = 'inner-card'
+            card.innerHTML = `
+                
+                <img src="${game.thumbnail}" alt="image" class="thumbnail">
+                <div class="m-4 relative">
+                    <span class="font-bold">${game.title}</span>
+                    <span class="block text-gray-500 text-sm">Platform: ${game.platform}</span>
+                    <span class="block text-gray-500 text-sm">Category: ${game.genre}</span>
+                    <span class="heart" id="heart">♡</span>
+
+                    <button class="card-button" id="view"><a href="#game">VIEW</a></button>
+                </div>
+                <div class="game-date">
+                    <span>${game.release_date}</span>
+                </div>
+            `
+            /* Event Listeners */
+            card.querySelector('button#view').addEventListener('click', e => {
+                    ///Pass the id to featured game to post the id
+                const id = game.id
+                    //Remove existing card and pass it to the function
+                document.querySelector('div.featured-game').remove()
+                featuredGame(id)
+            })
+                //Swicth hearts when user selects
+            card.querySelector('span#heart').addEventListener('click', e => {
+                let heartSwitcher = {
+                    "♡" : "❤",
+                    "❤" : "♡",
+                    '' : 'red',
+                    'red' : ''
+                }
+                e.target.textContent = heartSwitcher[e.target.innerText]
+                e.target.style.color = heartSwitcher[e.target.style.color]
+            })
+            gameCard.appendChild(card)
+            // console.log(game)
+        })
+    })
+}
+    //This function gets all give aways from api URL and displays them  
+function giveAways(){
+    const url = `${apiURL}giveaways`
+    const method = 'GET'
+
+    const giveaways = processData(url,method)
+        //Promise returns. Display the giveaways on side menu
+    const givecards = document.querySelector('div#giveaways')
+    giveaways.then(res => {
+        res.forEach(game => {
+            const card = document.createElement('div')
+            card.innerHTML = `
+                <div class="give-card">
+                <img src="${game.thumbnail}" alt="img1" class="w-1/4">
+                <p class="truncate p-2">
+                    <a href="${game.giveaway_url}">${game.title} target="_blank"</a>
+                    </p>
+                </div>
+                <div class="progress-bar">
+                    <div class="progress" style="width:${game.keys_left}"></div>
+                </div>
+            `
+            givecards.appendChild(card)
+        })
+    })
+}
+    //This function first displays a random game, it also handles a game that the user has selected
 async function featuredGame(id=null){
     let indx
         //Display a random game from the API
-        //If sessionStorage is '' display random game . If it is set - Display that selected by user
     let url = `${apiURL}games`
     const method = 'GET'
     if(id!==null){
@@ -82,96 +176,6 @@ async function featuredGame(id=null){
         featured.appendChild(fGame)
     })
 }
-    /* Game Cards that display individual games */
-    /* This function will display game cards, and also sort according to platform */
-
-function displayGameCards(platform=null){
-        //By default this function displays all games from index 0 - 395 in the main html
-        //We will add an event listener to the form in thw hero section that will take the value of the platform
-        //That value will be received by this function, thus the url changes and options value for processData will be passed
-    const method = 'GET'
-    let url
-    let gamesList
-        //If platform is null i.e has a no value, allow it to fetch all games from index 0 - 395
-    if(platform !== null){
-        let options = platform
-        url = `${apiURL}games?platform=${platform}`
-        gamesList = processData(url,method,options)
-    }else{
-        url = `${apiURL}games`
-        gamesList = processData(url,method)
-            //If platform has a value i.e pc or browser. Set options to the platform, change the url to accept platform, pass value and fetch
-            //to get a sorted list according to value
-    }
-        //Display games on the main page
-    const gameCard = document.querySelector('div#gamecard')
-    gamesList.then(games => {
-        games.forEach(game => {
-            const card = document.createElement('div')
-            card.className = 'inner-card'
-            card.innerHTML = `
-                
-                <img src="${game.thumbnail}" alt="image" class="thumbnail">
-                <div class="m-4 relative">
-                    <span class="font-bold">${game.title}</span>
-                    <span class="block text-gray-500 text-sm">Platform: ${game.platform}</span><span class="heart" id="heart">♡</span>
-
-                    <button class="card-button" id="view"><a href="#game">VIEW</a></button>
-                </div>
-                <div class="game-date">
-                    <span>${game.release_date}</span>
-                </div>
-            `
-            /* Event Listeners */
-            card.querySelector('button#view').addEventListener('click', e => {
-                    ///Pass the id to featured game to post the id
-                const id = game.id
-                    //Remove existing card and pass it to the function
-                document.querySelector('div.featured-game').remove()
-                featuredGame(id)
-            })
-                //Swicth hearts when user selects
-            card.querySelector('span#heart').addEventListener('click', e => {
-                let heartSwitcher = {
-                    "♡" : "❤",
-                    "❤" : "♡",
-                    '' : 'red',
-                    'red' : ''
-                }
-                e.target.textContent = heartSwitcher[e.target.innerText]
-                e.target.style.color = heartSwitcher[e.target.style.color]
-            })
-            gameCard.appendChild(card)
-            // console.log(game)
-        })
-    })
-}
-    //This function gets all give aways from api URL and displays them  
-function giveAways(){
-    const url = `${apiURL}giveaways`
-    const method = 'GET'
-
-    const giveaways = processData(url,method)
-        //Promise returns. Display the giveaways on side menu
-    const givecards = document.querySelector('div#giveaways')
-    giveaways.then(res => {
-        res.forEach(game => {
-            const card = document.createElement('div')
-            card.innerHTML = `
-                <div class="give-card">
-                <img src="${game.thumbnail}" alt="img1" class="w-1/4">
-                <p class="truncate p-2">
-                    <a href="${game.giveaway_url}">${game.title} target="_blank"</a>
-                    </p>
-                </div>
-                <div class="progress-bar">
-                    <div class="progress" style="width:${game.keys_left}"></div>
-                </div>
-            `
-            givecards.appendChild(card)
-        })
-    })
-}
 /************** REUSABLE FUNCTIONS ****************************/
     //FUNCTION THAT FETCHES API DATA and returns 
 async function processData(url,method,options=null){
@@ -200,10 +204,11 @@ async function processData(url,method,options=null){
 document.querySelector('form#myform').addEventListener('submit', e => {
     e.preventDefault()
     const platform = e.target.platform.value
+    const category = e.target.category.value
         //Clear the existing display
     document.querySelector('div#gamecard').innerHTML = ''
         //Pass the value to our displayCards()
-    displayGameCards(platform)
+    displayGameCards(platform,category)
     document.querySelector('form#myform').reset()
 })
 /******************** MAIN EVENT LISTENER *********************/
